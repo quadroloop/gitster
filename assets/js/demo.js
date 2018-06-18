b3e8dbbc8498c5937484228015d1e72c2937d0eb
quadroloop <nplixel@gmail.com>
2018-06-18 17:52:10 +0300

    added basic functionalities and modified UI

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -130,22 +130,7 @@ demo = {
         marker.setMap(map);
     },
 
-	showNotification: function(from, align){
-    	color = Math.floor((Math.random() * 4) + 1);
-
-    	$.notify({
-        	icon: "ti-gift",
-        	message: "Welcome to <b>Paper Dashboard</b> - a beautiful freebie for every web developer."
-
-        },{
-            type: type[color],
-            timer: 4000,
-            placement: {
-                from: from,
-                align: align
-            }
-        });
-	},
+	
     initDocumentationCharts: function(){
     //     	init single simple line chart
         var dataPerformance = {
--- assets/js/gitster.js
+++ assets/js/gitster.js
@@ -2,3 +2,16 @@
 // Bryce Mercines 2018
 
 var user = 'quadroloop';
+
+window.onload = function() {
+	axios.get('https://api.github.com/users/'+user)
+	.then((res)=>{
+      document.getElementById('avatar').src = res.data.avatar_url;
+      document.getElementById('name').innerHTML = res.data.name;
+      document.getElementById('login').innerHTML = res.data.login;
+      document.getElementById('bio').innerHTML = res.data.bio;
+	})
+	.catch((err)=>{
+
+	});
+}
